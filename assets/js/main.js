ed9e2b87979d3dd32df7ea0d2a33a7489b0eaf01
mecarm <melecarmine99@gmail.com>
2022-10-18 15:28:18 +0200

    Esercizio completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,5 @@ let color = prompt("Scrivi qui il tuo colore preferito")
 
 let password = name + surname + color + 21
 console.log(password)
+
+document.getElementById('Password').innerHTML = 'Questa è la insicurissima password per te generata : ' + password

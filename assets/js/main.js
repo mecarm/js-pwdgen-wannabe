98fa2866d155c5ae9d8aa715d8b44b6f70fa5919
mecarm <melecarmine99@gmail.com>
2022-10-18 16:50:07 +0200

    Creo una mia versione custom agg. una function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+//Trasformo la prima versione del esercizio in un commento per sviluppare una mia versione custom
+/*
 let name = prompt("Scrivi qui il tuo nome")
 let surname = prompt("Scrivi qui il tuo cognome")
 let color = prompt("Scrivi qui il tuo colore preferito")
@@ -6,3 +8,15 @@ let password = name + surname + color + 21
 console.log(password)
 
 document.getElementById('Password').innerHTML = 'Questa è la insicurissima password per te generata : ' + password
+*/
+
+function myPassword() {
+    let name = prompt("Scrivi qui il tuo nome");
+    let surname = prompt("Scrivi qui il tuo cognome");
+    let color = prompt("Scrivi qui il tuo colore preferito");
+    let password = name + surname + color + 21
+    document.getElementById('Password').innerHTML = 'Questa è la insicurissima password per te generata 🤌: ' + password
+}
+
+
+

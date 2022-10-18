b8ca58bae4fa151f327e8448f9858006d69e9c2c
mecarm <melecarmine99@gmail.com>
2022-10-18 15:24:48 +0200

    Creo variabili

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let name = prompt("Scrivi qui il tuo nome")
+let surname = prompt("Scrivi qui il tuo cognome")
+let color = prompt("Scrivi qui il tuo colore preferito")
+
+let password = name + surname + color + 21
+console.log(password)
